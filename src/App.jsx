813f0827b80fb14bb7b4bbd046d572c882076a29
vinij-03 <vineetjana3@gmail.com>
2024-12-20 23:24:19 +0530

    fix: update axios baseURL to remove trailing slash

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { UserContextProvider } from './UserContext'
 
 function App() {
 
-  axios.defaults.baseURL = 'https://psychological-issy-vineetjana-6c8595a2.koyeb.app/';
+  axios.defaults.baseURL = 'https://psychological-issy-vineetjana-6c8595a2.koyeb.app';
 
   axios.defaults.withCredentials = true;
 
